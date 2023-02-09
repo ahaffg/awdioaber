42ab5ad7038a7fc87855507a345606f5322c80ed
ahaffg <angharad-fflur@live.co.uk>
2023-02-09 12:18:04 +0000

    removing code to allow for sizes as unnecessary with my products

--- bag/views.py
+++ bag/views.py
@@ -18,36 +18,14 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
     bag = request.session.get('bag', {})
-
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request,
-                                 (f'Updated size {size.upper()} '
-                                  f'{product.name} quantity to '
-                                  f'{bag[item_id]["items_by_size"][size]}'))
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request,
-                                 (f'Added size {size.upper()} '
-                                  f'{product.name} to your bag'))
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request,
-                             (f'Added size {size.upper()} '
-                              f'{product.name} to your bag'))
-    else:
-        if item_id in list(bag.keys()):
+    
+    if item_id in list(bag.keys()):
             bag[item_id] += quantity
             messages.success(request,
                              (f'Updated {product.name} '
                               f'quantity to {bag[item_id]}'))
-        else:
+    else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
 
@@ -61,31 +39,12 @@ def adjust_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    bag = request.session.get('bag', {})
-
-    if size:
-        if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request,
-                             (f'Updated size {size.upper()} '
-                              f'{product.name} quantity to '
-                              f'{bag[item_id]["items_by_size"][size]}'))
-        else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request,
-                             (f'Removed size {size.upper()} '
-                              f'{product.name} from your bag'))
-    else:
-        if quantity > 0:
+    if quantity > 0:
             bag[item_id] = quantity
             messages.success(request,
                              (f'Updated {product.name} '
                               f'quantity to {bag[item_id]}'))
-        else:
+    else:
             bag.pop(item_id)
             messages.success(request,
                              (f'Removed {product.name} '
