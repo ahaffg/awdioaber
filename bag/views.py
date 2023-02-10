2eb4499ff2b56f5142b468710bd7626912740e76
ahaffg <angharad-fflur@live.co.uk>
2023-02-10 13:19:30 +0000

    added css rules to toasts, and updated toast templates

--- bag/views.py
+++ bag/views.py
@@ -40,13 +40,13 @@ def adjust_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     size = None
     if quantity > 0:
-            bag[item_id] = quantity
-            messages.success(request,
+        bag[item_id] = quantity
+        messages.success(request,
                              (f'Updated {product.name} '
                               f'quantity to {bag[item_id]}'))
     else:
-            bag.pop(item_id)
-            messages.success(request,
+        bag.pop(item_id)
+        messages.success(request,
                              (f'Removed {product.name} '
                               f'from your bag'))
 
@@ -56,28 +56,12 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
+    bag.pop(item_id)
+    messages.success(request, f'Removed {product.name} from your bag')
 
-    try:
-        product = get_object_or_404(Product, pk=item_id)
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        bag = request.session.get('bag', {})
-
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-            messages.success(request,
-                             (f'Removed size {size.upper()} '
-                              f'{product.name} from your bag'))
-        else:
-            bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
-
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
 
     except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        return HttpResponse(status=500)
+    messages.error(request, f'Error removing item: {e}')
+    return HttpResponse(status=500)
