76b43da3437e3d4a8c21708e52d0d1e939e76dc3
ahaffg <angharad-fflur@live.co.uk>
2023-02-10 11:17:00 +0000

    adding toast functionality and updating bae.html

--- awdio_aber/settings.py
+++ awdio_aber/settings.py
@@ -15,8 +15,8 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
