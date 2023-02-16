c71ee1d255223e8b224e0d805f9ea481447dfcc0
ahaffg <angharad-fflur@live.co.uk>
2023-02-16 13:25:38 +0000

    trying to fix issues with models.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import category
+from .models import Category
 
 # Register your models here.
 
@@ -20,5 +20,12 @@ class BlogCategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-admin.site.register(Article)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(ArticleAdmin)
 admin.site.register(Blog_category)
+admin.site.register(BlogAdmin)
