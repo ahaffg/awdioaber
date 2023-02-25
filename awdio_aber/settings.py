bcf7726d93b83d3c91306acee211f4411d7efc49
ahaffg <angharad-fflur@live.co.uk>
2023-02-25 16:12:47 +0000

    correcting proxy error issue

--- awdio_aber/settings.py
+++ awdio_aber/settings.py
@@ -125,14 +125,16 @@ WSGI_APPLICATION = 'awdio_aber.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse('DATABASE_URL')
     }
-else: DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
 
 
 # Password validation
