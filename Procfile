188ba18c016028a9d2ad1d57ac07f9adf14e2ced
ahaffg <angharad-fflur@live.co.uk>
2023-02-27 15:22:21 +0000

    adding deployment settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn awdio_aber.wsgi:application
--- awdio_aber/settings.py
+++ awdio_aber/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-52+5fipl%i^u%2(@88xga=^hl4@b)_slk-a!-u@66evfaza@l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['awdioaber.herokuapp.com']
 
 
 # Application definition
@@ -114,7 +114,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
