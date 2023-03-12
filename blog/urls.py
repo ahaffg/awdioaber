553e3be6c3431e167c76bec058782305afc38f3f
ahaffg <angharad-fflur@live.co.uk>
2023-03-12 21:52:36 +0000

    updating blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_articles, name='blog'),
-    path('article/', views.article, name='article')
+    path('<int:article_id>/', views.article, name='article'),
+    path('add/', views.add_product, name='add_article'),
+    path('edit/<int:article_id>/', views.edit_article, name='edit_article'),
+    path('delete/<int:article_id>/', views.delete_article, name='delete_article'),
 ]
