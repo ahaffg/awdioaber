3ebc393f7bcdc3c1bbdd7e834c4d8f2ca4ae3151
ahaffg <angharad-fflur@live.co.uk>
2023-03-15 19:22:48 +0000

    changes to blog templates

--- awdio_aber/urls.py
+++ awdio_aber/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
+    path('blogs/', include('blog.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
