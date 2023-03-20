d04f6212ccfc06defc2a52db8247ba952ace5e64
ahaffg <angharad-fflur@live.co.uk>
2023-03-20 11:45:39 +0000

    updating information regarding checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -141,8 +141,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MZzM9EQhK7XUVQZqhCG7R6eufg2EPEOWcZPICbBzQFMpdOp5Cfy4Ho4IpGJYQHyWKf9OVWrzjM3z77LsgqPgrti00TNVEqbUI',
-        'client_secret': 'test client secret',
+        'stripe_public_key': 'stripe_public_key',
+        'client_secret': 'intent.client_secret',
     }
 
     return render(request, template, context)
